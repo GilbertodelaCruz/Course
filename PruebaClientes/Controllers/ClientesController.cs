71d6c2b3367a2bf8b980806a44c439aa5d5511fc
GilbertoDLCS <38504430+GilbertodelaCruz@users.noreply.github.com>
2019-01-14 21:49:01 -0600

    Agregar archivos de proyecto.

---
+++ PruebaClientes/Controllers/ClientesController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PruebaClientes.Models;
+
+namespace PruebaClientes.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private EmptyDBContext db = new EmptyDBContext();
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            var Clientes  = from e in db.Clientes
+                                          orderby e.ID
+                                          select e;
+            return View(Clientes);
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clientes/Create
+        [HttpPost]
+        public ActionResult Create(Clientes NuevoCliente)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Clientes.Add(NuevoCliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PruebaClientes/Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PruebaClientes.Models
+{
+    public class Clientes
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+    }
+
+    public class EmptyDBContext: DbContext {
+        public EmptyDBContext() { }
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+}
